7fdfbb0ba202e0c7c585a3d0c8db866cc93cf2e1
Kunal Sawle <ksawle1515@gmail.com>
2025-10-08 04:30:41 +0530

    commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from 'express';  
+import cors from 'cors';
+import 'dotenv/config';
+import { connectDB } from './config/db.js';
+
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+//Connected to database
+
+connectDB();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server running on adress http://localhost:${PORT}`);
+})
