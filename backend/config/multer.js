e60ca2d4cff4616d8210bdf1e26230059deb9281
Kunal Sawle <ksawle1515@gmail.com>
2025-10-24 20:11:21 +0530

    add multer for image api

---
+++ backend/config/multer.js
@@ -0,0 +1,9 @@
+import e from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({});
+
+ const upload = multer({ storage });
+
+ export default upload;
