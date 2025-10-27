719b372342425906e63ba73dacd8e388bcdf1b18
Kunal Sawle <ksawle1515@gmail.com>
2025-10-28 01:32:11 +0530

    add banner in commponents

---
+++ frontend/src/components/home/banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const banner = () => {
+  return (
+    <div>
+      <div
+        className="w-full py-2.5 font-medium text-sm text-white text-center 
+        bg-gradient-to-r from-violet-500 via-[#9938CA] to-[#E0724A]"
+      >
+        <p><span>AI Powered Resume Builder</span></p>
+      </div>
+    </div>
+  );
+};
+
+export default banner;
