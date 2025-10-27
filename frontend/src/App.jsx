54af2eab150e29a3b98cdd5e4eaa6bae213e9426
Kunal Sawle <ksawle1515@gmail.com>
2025-10-28 01:35:13 +0530

    correct structure for route from react route dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Layout from "./pages/Layout.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
 import Preview from "./pages/Preview";
-import Login from "./pages/Login.jsx";
-import ResumeBuilder from "./pages/ResumeBuilder.jsx";
+import Login from "./pages/Login";
+import ResumeBuilder from "./pages/ResumeBuilder";
 
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="app" element={<Layout />} />
-        <Route index element={<Dashboard />} />
-        <Route path="builder/:resumeId" element={<ResumeBuilder />} />
-        <Route path='view/:resumeId' element={<Preview/>}/>
-        <Route path='login' element={<Login/>}/>
+          {/* Root route */}
+        <Route path='/' element={<Home />} />
 
+        {/* Nested routes under /app */}
+        <Route path='app' element={<Layout />} >
+        <Route index element={<Dashboard />} />
+        <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+        </Route>
+
+        <Route path='view/:resumeId' element={<Preview/>}/>
+        
+         {/* Login route */}
+        <Route path='login' element={<Login/>}/>
       </Routes>
 
     </>
