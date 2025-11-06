78c7a973a850dc2325035a9efb3f7b06008d9eff
Kunal Sawle <ksawle1515@gmail.com>
2025-11-07 03:05:18 +0530

    correct event on Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -158,7 +158,7 @@ function Dashboard() {
               <h2 className="text-xl font-bold mb-4 text-white-400">
                 Create a Resume
               </h2>
-              <input onChange={()=>setTitle(e.target.value)} value={title}
+              <input onChange={(e)=>setTitle(e.target.value)} value={title}
                 type="text"
                 placeholder="Enter resume title"
                 className="w-full px-4 py-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-red-600"
