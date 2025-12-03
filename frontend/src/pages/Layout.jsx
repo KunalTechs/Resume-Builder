f978da72b028c60a8a68eae76d61656782ebc84e
Kunal Sawle <ksawle1515@gmail.com>
2025-12-03 23:43:24 +0530

    add Login page direct after home page when getstarted click

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import {useSelector} from 'react-redux'
+import Loader from '../components/Loader'
+import Login from './Login';
 
 const Layout = () => {
+
+const {user, loading} = useSelector(state => state.auth)
+
+if(loading){
+  return <Loader />
+}
+
   return (
     <div>
-      <div className='min-h-screen bg-[#0A0A0A] shadow-lg'>
+      {
+        user ? (
+           <div className='min-h-screen bg-[#0A0A0A] shadow-lg'>
         <Navbar/>
         <Outlet/>
       </div>
+        )
+        : <Login />
+      }
+     
     </div>
   )
 }
